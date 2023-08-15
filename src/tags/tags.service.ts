2ec81761e2d5dcb643ac6eed83c4ee730766e38c
hehe1111 <2908749709@qq.com>
2023-08-15 14:32:19 +0800

    style(src/tags/tag.service.ts): 删除无用注释

--- src/tags/tags.service.ts
+++ src/tags/tags.service.ts
@@ -6,9 +6,6 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm'
 
 // TODO: 错误处理
-// name 过长
-// sign 过长
-// 必填字段为空
 // 要更新的数据不存在
 // 要更新的数据已被删除，不能再更新
 // 重复删除
@@ -35,21 +32,10 @@ export class TagsService {
 
   findOne(id: number) {
     return this.tagRepository.findOneBy({ id })
-    // 等价于
-    // return this.tagRepository.findOne({ where: { id } })
   }
 
   async update(id: number, updateTagDto: UpdateTagDto) {
-    // save：数据库不存在该数据，则新增，否则更新
     await this.tagRepository.save({ id, ...updateTagDto })
-    // 成功结果示例
-    // {
-    //   "id": 2,
-    //   "name": "游泳 2==",
-    //   "kind": 1,
-    //   "deletedAt": null,
-    //   "updatedAt": "2023-08-13T04:23:41.000Z"
-    // }
     // ! save 只返回了有变动的部分字段，故需要重新查找
     return this.tagRepository.findOneBy({ id })
   }
