e769d61aa2b440802a82f10e79f97c0a9fa0ac31
hehe1111 <2908749709@qq.com>
2023-08-15 14:25:44 +0800

    feat(src/main.ts): 添加 api/v1 路径前缀

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { RequestMethod } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // https://docs.nestjs.com/faq/global-prefix
+  app.setGlobalPrefix('api/v1', {
+    // exclude: [{ path: 'health', method: RequestMethod.GET }],
+  });
   await app.listen(3000);
 }
 bootstrap();
