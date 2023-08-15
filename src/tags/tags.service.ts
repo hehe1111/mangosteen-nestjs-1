9b4668f305b2a4b9d0d7ddaf9ec9503f7f2dc140
hehe1111 <2908749709@qq.com>
2023-08-15 15:47:25 +0800

    feat(src/tags/tags.service.ts): 处理边界情况

--- src/tags/tags.service.ts
+++ src/tags/tags.service.ts
@@ -1,14 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { CreateTagDto } from './dto/create-tag.dto';
 import { UpdateTagDto } from './dto/update-tag.dto';
 import { TagEntity } from './entities/tag.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm'
 
-// TODO: 错误处理
-// 要更新的数据不存在
-// 要更新的数据已被删除，不能再更新
-// 重复删除
 @Injectable()
 export class TagsService {
   @InjectRepository(TagEntity)
@@ -19,7 +15,7 @@ export class TagsService {
   }
 
   findAll() {
-    return this.tagRepository.find()
+    return this.tagRepository.find({ withDeleted: true })
   }
 
   findOne(id: number) {
@@ -27,12 +23,28 @@ export class TagsService {
   }
 
   async update(id: number, updateTagDto: UpdateTagDto) {
+    // exist 会自动过滤 deletedAt 非空的数据
+    const existed = await this.tagRepository.exist({ where: { id } })
+    if (!existed) {
+      throw new HttpException('不能更新不存在的数据', 400)
+    }
+
     await this.tagRepository.save({ id, ...updateTagDto })
     // ! save 只返回了有变动的部分字段，故需要重新查找
     return this.tagRepository.findOneBy({ id })
   }
 
-  remove(id: number) {
+  async remove(id: number) {
+    const record = await this.tagRepository.findOneBy({ id })
+
+    if (!record) {
+      throw new HttpException('不能删除不存在的数据', 400)
+    }
+
+    if (record.deletedAt) {
+      throw new HttpException('重复删除', 400)
+    }
+
     return this.tagRepository.softDelete(id)
   }
 }
