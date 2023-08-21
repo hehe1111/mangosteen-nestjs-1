4fe1e4c8d9f35a99df44664ccbe854a10efd66b6
hehe1111 <2908749709@qq.com>
2023-08-21 19:04:59 +0800

    refactor: 从 .env 文件读取数据库密码等配置，避免硬编码

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,31 +6,43 @@ import { TagsModule } from './tags/tags.module';
 import { UsersModule } from './users/users.module';
 import { SessionModule } from './session/session.module';
 import { RedisModule } from './redis/redis.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '111111',
-      database: 'mangosteen',
-      synchronize: true, // Note: This option auto-creates tables (don't use in production)
-      logging: true,
-      // ! 必须要用 .js 不能用 .ts
-      // ! 否则会报 `EntityMetadataNotFoundError: No metadata for "TagEntity" was found.`
-      // https://stackoverflow.com/a/64128240
-      entities: [__dirname + '/**/*.entity.js'],
-      migrations: [],
-      subscribers: [],
-      poolSize: 10,
-      connectorPackage: 'mysql2',
-      // TODO: 以下配置报 warning：
-      // Ignoring invalid configuration option passed to Connection: authPlugin.This is currently a warning, but in future versions of MySQL2, an error will be thrown if you pass an invalid configuration option to a Connection
-      // extra: {
-      //   authPlugin: 'sha256_password',
-      // },
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `${__dirname}/.env`,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory(configService: ConfigService) {
+        return {
+          type: 'mysql',
+          host: configService.get<string>('MYSQL_HOST'),
+          port: configService.get<number>('MYSQL_PORT'),
+          username: configService.get<string>('MYSQL_USERNAME'),
+          password: configService.get<string>('MYSQL_PASSWORD'),
+          database: configService.get<string>('MYSQL_DATABASE'),
+          synchronize: true, // Note: This option auto-creates tables (don't use in production)
+          logging: true,
+          // ! 必须要用 .js 不能用 .ts
+          // ! 否则会报 `EntityMetadataNotFoundError: No metadata for "TagEntity" was found.`
+          // https://stackoverflow.com/a/64128240
+          entities: [__dirname + '/**/*.entity.js'],
+          migrations: [],
+          subscribers: [],
+          poolSize: 10,
+          connectorPackage: 'mysql2',
+          // TODO: 以下配置报 warning：
+          // Ignoring invalid configuration option passed to Connection: authPlugin.This is currently a warning, but in future versions of MySQL2, an error will be thrown if you pass an invalid configuration option to a Connection
+          // extra: {
+          //   authPlugin: 'sha256_password',
+          // },
+        }
+      },
+
     }),
     TagsModule,
     UsersModule,
