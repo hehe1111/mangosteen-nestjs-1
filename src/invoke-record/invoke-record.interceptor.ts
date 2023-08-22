e3656a571ccfa69a414933d2f83542f1e20633b9
hehe1111 <2908749709@qq.com>
2023-08-22 15:15:59 +0800

    feat: 使用拦截器记录请求、响应信息

--- src/invoke-record/invoke-record.interceptor.ts
+++ src/invoke-record/invoke-record.interceptor.ts
@@ -1,9 +1,26 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
 
 @Injectable()
 export class InvokeRecordInterceptor implements NestInterceptor {
+  private readonly logger = new Logger(InvokeRecordInterceptor.name)
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle();
+    const request = context.switchToHttp().getRequest()
+    const response = context.switchToHttp().getResponse()
+
+    const userAgent = request.headers['user-agent']
+    const { ip, method, path } = request
+    const target = `${ip} ${userAgent} ${method} ${path}: `
+
+    this.logger.debug(`${target}${context.getClass().name}#${context.getHandler().name} invoked...`)
+    // this.logger.debug(`user: ${request.user?.userId}, ${request.user?.username}`);
+
+    const now = Date.now()
+
+    return next.handle().pipe(tap(res => {
+      this.logger.debug(`${target}${response.statusCode} [${Date.now() - now}ms]`)
+      this.logger.debug(`Response: ${JSON.stringify(res)}`)
+    }));
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FormatResponseInterceptor } from './format-response/format-response.interceptor';
 import { ConfigService } from '@nestjs/config';
+import { InvokeRecordInterceptor } from './invoke-record/invoke-record.interceptor';
 // import { RequestMethod } from '@nestjs/common';
 
 async function bootstrap() {
@@ -13,6 +14,7 @@ async function bootstrap() {
     // exclude: [{ path: 'health', method: RequestMethod.GET }],
   });
   app.useGlobalInterceptors(new FormatResponseInterceptor())
+  app.useGlobalInterceptors(new InvokeRecordInterceptor())
 
   await app.listen(configService.get<number>('NESTJS_PORT'));
 }
