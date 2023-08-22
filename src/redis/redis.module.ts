19c0dc2a4b57542acaa2cfa8cca7ead48298f005
hehe1111 <2908749709@qq.com>
2023-08-22 13:51:21 +0800

    feat(src/redis/): 配置 redis 链接；完成读写 redis 的工具函数

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,7 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { createClient } from 'redis';
 import { RedisService } from './redis.service';
 
 @Module({
-  providers: [RedisService]
+  providers: [
+    {
+      provide: 'REDIS_CLIENT',
+      inject: [ConfigService],
+      async useFactory(configService: ConfigService) {
+        const client = createClient({
+          socket: {
+            host: configService.get<string>('REDIS_HOST'),
+            port: configService.get<number>('REDIS_PORT'),
+          },
+          database: configService.get<number>('REDIS_DATABASE'),
+        });
+        await client.connect();
+        return client;
+      },
+    },
+    RedisService,
+  ],
+  exports: [RedisService],
 })
 export class RedisModule {}
