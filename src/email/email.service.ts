947c29b8d877cdb414cf8445ad81028216c22c09
hehe1111 <2908749709@qq.com>
2023-08-22 13:53:11 +0800

    feat(src/email/): 配置 nodemailer；完成发送邮件工具函数

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { createTransport, Transporter } from 'nodemailer';
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+  private transporter: Transporter;
+
+  constructor(private configService: ConfigService) {
+    this.transporter = createTransport({
+      host: this.configService.get<string>('NODEMAILER_HOST'),
+      port: this.configService.get<number>('NODEMAILER_PORT'),
+      secure: false,
+      auth: {
+        user: this.configService.get<string>('NODEMAILER_EMAIL_FROM'),
+        pass: this.configService.get<string>('NODEMAILER_AUTHORIZATION_CODE'),
+      },
+    });
+  }
+
+  async sendMail({ to, subject, html }) {
+    await this.transporter.sendMail({
+      from: {
+        name: '记账',
+        address: this.configService.get<string>('NODEMAILER_EMAIL_FROM'),
+      },
+      to,
+      subject,
+      html,
+    });
+  }
+}
