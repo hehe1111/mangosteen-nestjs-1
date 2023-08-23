f3ae7218e6926210b132a7727d40a64c6255c156
hehe1111 <2908749709@qq.com>
2023-08-23 20:56:34 +0800

    feat(src/invoke-record/invoke-record.interceptor.ts): 日志中显示用户 ID

--- src/invoke-record/invoke-record.interceptor.ts
+++ src/invoke-record/invoke-record.interceptor.ts
@@ -24,7 +24,7 @@ export class InvokeRecordInterceptor implements NestInterceptor {
         context.getHandler().name
       } invoked...`,
     );
-    // this.logger.debug(`user: ${request.user?.userId}, ${request.user?.username}`);
+    this.logger.debug(`user: ${request.user?.userId}`);
 
     const now = Date.now();
 
