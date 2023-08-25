c2b16422335541918b51fd304420a20425593e42
hehe1111 <2908749709@qq.com>
2023-08-25 20:30:07 +0800

    doc(src/items/dto/update-item.dto.ts): 解决生成接口文档时，PartialType 不能正确提取 TS 类型的问题

--- src/items/dto/update-item.dto.ts
+++ src/items/dto/update-item.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
+// import { PartialType } from '@nestjs/mapped-types'; // 生成文档时，会生成空对象
+// https://docs.nestjs.com/openapi/mapped-types#partial
+import { PartialType } from '@nestjs/swagger'; // 生成文档时，可以正确生成 TS 类型
 import { CreateItemDto } from './create-item.dto';
 
 export class UpdateItemDto extends PartialType(CreateItemDto) {}
